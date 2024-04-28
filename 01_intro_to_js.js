808ab06e44a55ff4076052e43358c2c6a328aabc
Sihab Sarar Chistee <60611053+Sihab-10@users.noreply.github.com>
2024-04-28 12:15:21 +0600

    js intro added with notes

---
+++ 01_intro_to_js.js
@@ -0,0 +1,9 @@
+// ECMAScript
+// ECMAScript is a standard in which javaScript is based. 
+// How to execute javaScript
+// 1. JavaScript can run in a browser
+// 2. Nodejs :
+// browser -> engine (v8) -> pick this engine -> put in to a c++ program 
+// -> node.js (execute javaScript without browser) -> created by Ryan Dahl
+// 3. yet another way to execute javaScript is by inserting it inside <script> tag of an HTML Document.
+console.log("Hello World");
