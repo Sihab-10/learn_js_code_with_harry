f370c951cac398ed6e4bcde9c11926c76cf4f712
Sihab Sarar Chistee <60611053+Sihab-10@users.noreply.github.com>
2024-04-29 12:37:10 +0600

    variables with notes added

---
+++ 02_variables_in_js/script.js
@@ -0,0 +1,35 @@
+/*
+JavaScript is a dynamically typed programming language
+meanig it can change your data in runtime
+
+Variables and Data Types in javaScript
+
+Variable : A variable is a container that stores a value. 
+
+*/ 
+
+console.log("This content contains some code")
+
+let a = 67 // a contains 67
+
+console.log(a)
+a = "harry"
+console.log(a)
+a = 3.4
+console.log(a)
+// this is an example of dynamically typed programming language because we can change the data in a runtime way. first we put a int value then we change it with a string value then again we change it with a floating value. 
+
+// Rules for choosing variable names
+/*
+-> letters, digits, underscores and & sign allowed
+
+-> Must begin with a $,_ or a letter
+
+-> JavaScript reserved words cannot be used as a variable name 
+
+-> Sihab & SiHAb are different variable (case sensitive)
+
+*/ 
+
+// let 8num2 = 45; this is not allowed this will throw an error
+// let var = 4; this is not allowed 
